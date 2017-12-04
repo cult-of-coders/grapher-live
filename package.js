ea9e4eef62da20be42bbfef2cf77e908abc6a572
Theodor Diaconu <diaconu.theodor@gmail.com>
2017-12-04 12:57:37 +0200

    bump to 1.3.0_1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cultofcoders:grapher-live',
-  version: '1.3.0',
+  version: '1.3.0_1',
   // Brief, one-line summary of the package.
   summary: 'Grapher Live - Visualize your queries in the browser',
   // URL to the Git repository containing the source code for this package.
